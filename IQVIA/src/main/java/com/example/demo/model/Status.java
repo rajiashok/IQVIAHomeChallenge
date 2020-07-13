a5ce377415bbc63cfa9bb4642e4b2944d4272d76
rajiashok <rajalakshmi.ashokkumar@smu.ca>
2020-07-13 17:35:58 -0300

    Commiting java files for home challenge

---
+++ IQVIA/src/main/java/com/example/demo/model/Status.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+public class Status {
+	int StatusCode;
+	String StatusMessage;
+	public int getStatusCode() {
+		return StatusCode;
+	}
+	public void setStatusCode(int statusCode) {
+		StatusCode = statusCode;
+	}
+	public String getStatusMessage() {
+		return StatusMessage;
+	}
+	public void setStatusMessage(String statusMessage) {
+		StatusMessage = statusMessage;
+	}
+	
+
+}
